c80ab767f91583947667e668c3cacd6c80f30f64
Praveenmuthu4 <praveenmuthu4@gmail.com>
2023-04-04 21:02:40 +0530

    changed localhost data

--- routes/router.js
+++ routes/router.js
@@ -1,247 +1,204 @@
 const express = require("express");
 const router = new express.Router();
 const userdb = require("../models/userSchema");
-var bcrypt = require('bcrypt');
+var bcrypt = require("bcrypt");
 const authenticate = require("../middleware/authenticate");
 const nodemailer = require("nodemailer");
-const jwt  = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 
-const keysecret = process.env.SECRET_KEY
-
-
-
-// email config
+const keysecret = process.env.SECRET_KEY;
 
 const transporter = nodemailer.createTransport({
-    service:"gmail",
-    auth:{
-        user:process.env.EMAIL,
-        pass:process.env.PASSWORD
-    }
-}) 
-
-
-// for user registration
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+});
 
 router.post("/register", async (req, res) => {
+  const { name, email, password, cpassword } = req.body;
 
-    const { name, email, password, cpassword } = req.body;
+  if (!name || !email || !password || !cpassword) {
+    res.status(422).json({ error: "fill all the details" });
+  }
 
-    if (!name || !email || !password || !cpassword) {
-        res.status(422).json({ error: "fill all the details" })
+  try {
+    const preuser = await userdb.findOne({ email: email });
+
+    if (preuser) {
+      res.status(422).json({ error: "This Email is Already Exist" });
+    } else if (password !== cpassword) {
+      res
+        .status(422)
+        .json({ error: "Password and Confirm Password Not Match" });
+    } else {
+      const finalUser = new userdb({
+        name,
+        email,
+        password,
+        cpassword,
+      });
+
+      const storeData = await finalUser.save();
+      res.status(201).json({ status: 201, storeData });
     }
-
-    try {
-
-        const preuser = await userdb.findOne({ email: email });
-
-        if (preuser) {
-            res.status(422).json({ error: "This Email is Already Exist" })
-        } else if (password !== cpassword) {
-            res.status(422).json({ error: "Password and Confirm Password Not Match" })
-        } else {
-            const finalUser = new userdb({
-                name, email, password, cpassword
-            });
-
-            // here password hasing
-
-            const storeData = await finalUser.save();
-
-            // console.log(storeData);
-            res.status(201).json({ status: 201, storeData })
-        }
-
-    } catch (error) {
-        res.status(422).json(error);
-        console.log("catch block error");
-    }
-
+  } catch (error) {
+    res.status(422).json(error);
+    console.log("catch block error");
+  }
 });
-
-
-
-
-// user Login
-
 router.post("/login", async (req, res) => {
-    console.log(req.body);
+  console.log(req.body);
 
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    if (!email || !password) {
-        res.status(422).json({ error: "fill all the details" })
-    }
+  if (!email || !password) {
+    res.status(422).json({ error: "fill all the details" });
+  }
 
-    try {
-       const userValid = await userdb.findOne({email:email});
+  try {
+    const userValid = await userdb.findOne({ email: email });
 
-        if(userValid){
+    if (userValid) {
+      const isMatch = await bcrypt.compare(password, userValid.password);
 
-            const isMatch = await bcrypt.compare(password,userValid.password);
-
-            if(!isMatch){
-                res.status(422).json({ error: "invalid details"})
-            }else{
-
-                // token generate
-                const token = await userValid.generateAuthtoken();
-
-                // cookiegenerate
-                res.cookie("usercookie",token,{
-                    expires:new Date(Date.now()+9000000),
-                    httpOnly:true
-                });
-
-                const result = {
-                    userValid,
-                    token
-                }
-                res.status(201).json({status:201,result})
-            }
-        }else{
-            res.status(401).json({status:401,message:"invalid details"});
-        }
-
-    } catch (error) {
-        res.status(401).json({status:401,error});
-        console.log("catch block");
-    }
-});
-
-
-
-// user valid
-router.get("/validuser",authenticate,async(req,res)=>{
-    try {
-        const ValidUserOne = await userdb.findOne({_id:req.userId});
-        res.status(201).json({status:201,ValidUserOne});
-    } catch (error) {
-        res.status(401).json({status:401,error});
-    }
-});
-
-
-// user logout
-
-router.get("/logout",authenticate,async(req,res)=>{
-    try {
-        req.rootUser.tokens =  req.rootUser.tokens.filter((curelem)=>{
-            return curelem.token !== req.token
+      if (!isMatch) {
+        res.status(422).json({ error: "invalid details" });
+      } else {
+        const token = await userValid.generateAuthtoken();
+        res.cookie("usercookie", token, {
+          expires: new Date(Date.now() + 9000000),
+          httpOnly: true,
         });
 
-        res.clearCookie("usercookie",{path:"/"});
-
-        req.rootUser.save();
-
-        res.status(201).json({status:201})
-
-    } catch (error) {
-        res.status(401).json({status:401,error})
+        const result = {
+          userValid,
+          token,
+        };
+        res.status(201).json({ status: 201, result });
+      }
+    } else {
+      res.status(401).json({ status: 401, message: "invalid details" });
     }
+  } catch (error) {
+    res.status(401).json({ status: 401, error });
+    console.log("catch block");
+  }
 });
-
-
-
-// send email Link For reset Password
-router.post("/sendpasswordlink",async(req,res)=>{
-    console.log(req.body)
-
-    const {email} = req.body;
-
-    if(!email){
-        res.status(401).json({status:401,message:"Enter Your Email"})
-    }
-
-    try {
-        const userfind = await userdb.findOne({email:email});
-
-        // token generate for reset password
-        const token = jwt.sign({_id:userfind._id},keysecret,{
-            expiresIn:"120s"
-        });
-        
-        const setusertoken = await userdb.findByIdAndUpdate({_id:userfind._id},{verifytoken:token},{new:true});
-
-
-        if(setusertoken){
-            const mailOptions = {
-                from:process.env.EMAIL,
-                to:email,
-                subject:"Sending Email For password Reset",
-                text:`This Link Valid For 2 MINUTES http://localhost:3001/forgotpassword/${userfind.id}/${setusertoken.verifytoken}`
-            }
-
-            transporter.sendMail(mailOptions,(error,info)=>{
-                if(error){
-                    console.log("error",error);
-                    res.status(401).json({status:401,message:"email not send"})
-                }else{
-                    console.log("Email sent",info.response);
-                    res.status(201).json({status:201,message:"Email sent Succsfully"})
-                }
-            })
-
-        }
-
-    } catch (error) {
-        res.status(401).json({status:401,message:"invalid user"})
-    }
-
+router.get("/validuser", authenticate, async (req, res) => {
+  try {
+    const ValidUserOne = await userdb.findOne({ _id: req.userId });
+    res.status(201).json({ status: 201, ValidUserOne });
+  } catch (error) {
+    res.status(401).json({ status: 401, error });
+  }
 });
+router.get("/logout", authenticate, async (req, res) => {
+  try {
+    req.rootUser.tokens = req.rootUser.tokens.filter((curelem) => {
+      return curelem.token !== req.token;
+    });
 
+    res.clearCookie("usercookie", { path: "/" });
 
-// verify user for forgot password time
-router.get("/forgotpassword/:id/:token",async(req,res)=>{
-    const {id,token} = req.params;
+    req.rootUser.save();
 
-    try {
-        const validuser = await userdb.findOne({_id:id,verifytoken:token});
-        
-        const verifyToken = jwt.verify(token,keysecret);
-
-        console.log(verifyToken)
-
-        if(validuser && verifyToken._id){
-            res.status(201).json({status:201,validuser})
-        }else{
-            res.status(401).json({status:401,message:"user not exist"})
-        }
-
-    } catch (error) {
-        res.status(401).json({status:401,error})
-    }
+    res.status(201).json({ status: 201 });
+  } catch (error) {
+    res.status(401).json({ status: 401, error });
+  }
 });
+router.post("/sendpasswordlink", async (req, res) => {
+  console.log(req.body);
 
+  const { email } = req.body;
 
-// change password
+  if (!email) {
+    res.status(401).json({ status: 401, message: "Enter Your Email" });
+  }
 
-router.post("/:id/:token",async(req,res)=>{
-    const {id,token} = req.params;
+  try {
+    const userfind = await userdb.findOne({ email: email });
+    const token = jwt.sign({ _id: userfind._id }, keysecret, {
+      expiresIn: "120s",
+    });
 
-    const {password} = req.body;
+    const setusertoken = await userdb.findByIdAndUpdate(
+      { _id: userfind._id },
+      { verifytoken: token },
+      { new: true }
+    );
 
-    try {
-        const validuser = await userdb.findOne({_id:id,verifytoken:token});
-        
-        const verifyToken = jwt.verify(token,keysecret);
+    if (setusertoken) {
+      const mailOptions = {
+        from: process.env.EMAIL,
+        to: email,
+        subject: "Sending Email For password Reset",
+        text: `This Link Valid For 2 MINUTES http://localhost:8000/forgotpassword/${userfind.id}/${setusertoken.verifytoken}`,
+      };
 
-        if(validuser && verifyToken._id){
-            const newpassword = await bcrypt.hash(password,12);
-
-            const setnewuserpass = await userdb.findByIdAndUpdate({_id:id},{password:newpassword});
-
-            setnewuserpass.save();
-            res.status(201).json({status:201,setnewuserpass})
-
-        }else{
-            res.status(401).json({status:401,message:"user not exist"})
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log("error", error);
+          res.status(401).json({ status: 401, message: "email not send" });
+        } else {
+          console.log("Email sent", info.response);
+          res
+            .status(201)
+            .json({ status: 201, message: "Email sent Succsfully" });
         }
-    } catch (error) {
-        res.status(401).json({status:401,error})
+      });
     }
-})
+  } catch (error) {
+    res.status(401).json({ status: 401, message: "invalid user" });
+  }
+});
+router.get("/forgotpassword/:id/:token", async (req, res) => {
+  const { id, token } = req.params;
 
+  try {
+    const validuser = await userdb.findOne({ _id: id, verifytoken: token });
 
+    const verifyToken = jwt.verify(token, keysecret);
+
+    console.log(verifyToken);
+
+    if (validuser && verifyToken._id) {
+      res.status(201).json({ status: 201, validuser });
+    } else {
+      res.status(401).json({ status: 401, message: "user not exist" });
+    }
+  } catch (error) {
+    res.status(401).json({ status: 401, error });
+  }
+});
+router.post("/:id/:token", async (req, res) => {
+  const { id, token } = req.params;
+
+  const { password } = req.body;
+
+  try {
+    const validuser = await userdb.findOne({ _id: id, verifytoken: token });
+
+    const verifyToken = jwt.verify(token, keysecret);
+
+    if (validuser && verifyToken._id) {
+      const newpassword = await bcrypt.hash(password, 12);
+
+      const setnewuserpass = await userdb.findByIdAndUpdate(
+        { _id: id },
+        { password: newpassword }
+      );
+
+      setnewuserpass.save();
+      res.status(201).json({ status: 201, setnewuserpass });
+    } else {
+      res.status(401).json({ status: 401, message: "user not exist" });
+    }
+  } catch (error) {
+    res.status(401).json({ status: 401, error });
+  }
+});
 
 module.exports = router;
